f5e44413392958c882f8e4d3ab615d9667e09207
sg <svirgibs@gmail.com>
2023-03-12 17:58:13 +0300

    pre commit

---
+++ person.py
@@ -0,0 +1,23 @@
+
+
+class Person:
+    def __init__(self, name, job=None, pay=0):
+        self.name = name
+        self.job = job
+        self.pay = pay
+
+    def last_name(self):
+        return self.name.split()[-1]
+
+    def give_raise(self, percent):
+        self.pay = int(self.pay * (1 + percent))
+
+
+if __name__ == '__main__':
+    bob = Person('Bob Smith')
+    sue = Person('Sue Jones', job='dev', pay=100000)
+    print(bob.name, bob.pay)
+    print(sue.name, sue.pay)
+    print(bob.last_name(), sue.last_name())
+    sue.give_raise(.10)
+    print(sue)
